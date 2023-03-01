b855dc9f2f2e58e54a90a4ac13860595a55c4373
alnnnnra <123537239+alnnnnra@users.noreply.github.com>
2023-03-01 20:41:07 +0600

    Add files via upload

---
+++ Lab5/src/app/categories.ts
@@ -0,0 +1,18 @@
+export const categories = [
+  {
+    id: 1,
+    name: 'Apple'
+  },
+  {
+    id: 2,
+    name: 'Home'
+  },
+  {
+    id: 3,
+    name: 'Beauty'
+  },
+  {
+    id: 4,
+    name: 'Others'
+  },
+];
---
+++ Lab5/src/app/products.ts
@@ -0,0 +1,186 @@
+export const products = [
+  {
+    id: 1,
+    name: 'Apple iPhone 14 Pro Max 256Gb',
+    price: 1564.05,
+    description: 'Smartphone Apple iPhone 14 Pro Max 256Gb purple',
+    rating: 5,
+    image: 'https://resources.cdn-kaspi.kz/shop/medias/sys_master/images/images/h1a/h61/62948780736542/apple-iphone-14-pro-max-128gb-fioletovyj-106363342-1.jpg',
+    secondImage: 'https://resources.cdn-kaspi.kz/shop/medias/sys_master/images/images/h36/hf1/62948781195294/apple-iphone-14-pro-max-128gb-fioletovyj-106363342-2.jpg',
+    link: 'https://kaspi.kz/shop/p/apple-iphone-14-pro-max-256gb-fioletovyi-106363342/?c=750000000#!/item',
+    categoryId: 1,
+
+  },
+  {
+    id: 2,
+    name: 'Apple iPhone 13 256Gb',
+    price: 1037.98,
+    description: 'Smartphone Apple iPhone 13 256Gb blue',
+    rating: 5,
+    image: 'https://resources.cdn-kaspi.kz/shop/medias/sys_master/images/images/h6c/hae/46392667242526/apple-iphone-13-256gb-sinij-102298380-1-Container.jpg',
+    secondImage: 'https://resources.cdn-kaspi.kz/shop/medias/sys_master/images/images/h71/h71/46392667635742/apple-iphone-13-256gb-sinij-102298380-3-Container.jpg',
+    link: 'https://kaspi.kz/shop/p/apple-iphone-13-256gb-sinii-102298380/?c=750000000#!/item',
+    categoryId: 1,
+  },
+  {
+    id: 3,
+    name: 'Blue sofa',
+    price: 243.88,
+    description: `Blue Sofa`,
+    image:
+      'https://resources.cdn-kaspi.kz/shop/medias/sys_master/images/images/h49/hce/50353683333150/arman-company-komfort-kvadrat-p36-seryj-102085936-1-Container.jpg',
+      secondImage:
+      'https://resources.cdn-kaspi.kz/shop/medias/sys_master/images/images/h20/hd1/50353683529758/arman-company-komfort-kvadrat-p36-seryj-102085936-2-Container.jpg',
+    link: 'https://kaspi.kz/shop/p/bulgyn-komfort-p36-divan-obivka-veljur-60x284x140-sm-sinii-102085936/?c=750000000#!/item',
+    rating: 5.0,
+    categoryId: 2,
+  },
+
+  {
+    id: 4,
+    name: 'MacBook Pro 16',
+    price: 5478.56,
+    description: `Apple MacBook Pro 16 MVVM2 Laptop Silver`,
+    image:
+      'https://resources.cdn-kaspi.kz/shop/medias/sys_master/images/images/h18/h2d/46665337995294/apple-macbook-pro-16-mvvm2-serebristyj-100397588-1-Container.jpg',
+    secondImage: 'https://www.mechta.kz/images/product/69725/17100000154_2-480.webp',
+    image3: 'https://www.mechta.kz/images/product/69725/17100000154_5-480.webp',
+    link: 'https://kaspi.kz/shop/p/apple-macbook-pro-16-mvvm2-serebristyi-100397588/?c=750000000#!/item',
+    rating: 5.0,
+    categoryId: 1,
+  },
+
+  {
+    id: 5,
+    name: 'Airpods Pro 2',
+    price: 267.33,
+    description: `Apple Airpods Pro 2nd generation headphones white`,
+    image:
+      'https://resources.cdn-kaspi.kz/shop/medias/sys_master/images/images/hba/hf8/62281477259294/apple-airpods-pro-2nd-generation-belyj-106362968-1.jpg',
+    secondImage:
+      'https://www.mechta.kz/export/1cbitrix/import_files/3d/3db2e093-3a26-11ed-a24a-005056b6dbd7-480.webp',
+    image3:
+      'https://www.mechta.kz/export/1cbitrix/import_files/3d/3db2e095-3a26-11ed-a24a-005056b6dbd7-480.webp',
+    link: 'https://kaspi.kz/shop/p/apple-airpods-pro-2nd-generation-belyi-106362968/?c=750000000#!/item',
+    rating: 5.0,
+    categoryId: 1,
+  },
+
+  {
+    id: 6,
+    name: 'Apple Watch Series 8 45mm',
+    price: 503.13,
+    description: `Smart Watch Apple Watch Series 8 45mm Aluminum Black`,
+    image:
+      'https://resources.cdn-kaspi.kz/shop/medias/sys_master/images/images/h1a/h69/63158968057886/apple-watch-series-8-41-mm-cernyj-106362847-1.jpg',
+    secondImage:
+      'https://www.mechta.kz/export/1cbitrix/import_files/3f/3fa7c0fd-3b26-11ed-a24a-005056b6dbd7-480.webp',
+    image3:
+      'https://www.mechta.kz/export/1cbitrix/import_files/3f/3fa7c0ff-3b26-11ed-a24a-005056b6dbd7-480.webp',
+    link: 'https://kaspi.kz/shop/p/apple-watch-series-8-45-mm-aluminum-chernyi-106362847/?c=750000000#!/item',
+    rating: 5.0,
+    categoryId: 1,
+  },
+
+  {
+    id: 7,
+    name: 'Dyson HS05 Airwrap Complete',
+    price: 1112.99,
+    description: `Dyson HS05 Airwrap Complete`,
+    image: 'https://cdn1.ozone.ru/s3/multimedia-t/wc1000/6387934661.jpg',
+    secondImage:
+      'https://resources.cdn-kaspi.kz/shop/medias/sys_master/images/images/hdd/h8c/63083545165854/dyson-hs05-airwrap-complete-fen-setka-1300-w-106790035-2.jpg',
+    image3: 'https://images.satu.kz/194436844_w640_h640_194436844.jpg',
+    link: 'https://kaspi.kz/shop/p/dyson-hs05-airwrap-complete-fen-schetka-1300-w-106790035/?c=750000000#!/itemhttps://kaspi.kz/shop/p/apple-macbook-pro-16-mvvm2-serebristyi-100397588/?c=750000000#!/item',
+    rating: 5.0,
+    categoryId: 3,
+  },
+
+  {
+    id: 8,
+    name: 'Sony Alpha ILCE-6400 Kit 18-135 мм OSS',
+    price: 1112.99,
+    description: `Sony Alpha ILCE-6400 Kit 18-135 мм OSS`,
+    image:
+      'https://resources.cdn-kaspi.kz/shop/medias/sys_master/images/images/h64/h11/32393356410910/sony-alpha-ilce-6400-kit-18-135-mm-oss-cernyj-100115206-1.jpg',
+    secondImage:
+      'https://resources.cdn-kaspi.kz/shop/medias/sys_master/images/images/h10/h79/32393359327262/sony-alpha-ilce-6400-kit-18-135-mm-oss-cernyj-100115206-2.jpg',
+    image3:
+      'https://resources.cdn-kaspi.kz/shop/medias/sys_master/images/images/ha6/ha9/32393492922398/sony-alpha-ilce-6400-kit-18-135-mm-oss-cernyj-100115206-3.jpg',
+    link: 'https://kaspi.kz/shop/p/sony-alpha-ilce-6400-kit-18-135-mm-oss-chernyi-100115206/?c=750000000#!/item',
+    rating: 5.0,
+    categoryId: 4,
+  },
+
+  {
+    id: 9,
+    name: 'Seven Art mirror',
+    price: 122.24,
+    description: `Seven Art mirror 00014 60x165 cm, wall, floor`,
+    image:
+      'https://resources.cdn-kaspi.kz/shop/medias/sys_master/images/images/h59/hd8/47415563976734/seven-art-zerkalo-00014-60x165-sm-nastennoe-napolnoe-102995498-1.jpg',
+    secondImage:
+      'https://resources.cdn-kaspi.kz/shop/medias/sys_master/images/images/h8f/h28/47415564435486/seven-art-zerkalo-00014-60x165-sm-nastennoe-napolnoe-102995498-2.jpg',
+    image3:
+      'https://resources.cdn-kaspi.kz/shop/medias/sys_master/images/images/hb0/ha0/47415565352990/seven-art-zerkalo-00014-60x165-sm-nastennoe-napolnoe-102995498-4.jpg',
+    link: 'https://kaspi.kz/shop/p/seven-art-zerkalo-00014-60x165-sm-nastennoe-napol-noe-102995498/?c=750000000#!/item',
+    rating: 5.0,
+    categoryId: 2,
+  },
+
+  {
+    id: 10,
+    name: 'Smart speaker Yandex Station Mini 2',
+    price: 98.81,
+    description: `Smart speaker Yandex Station Mini 2 blue`,
+    image:
+      'https://resources.cdn-kaspi.kz/shop/medias/sys_master/images/images/h34/h6e/49342277877790/yandeks-mini-2-yndx-00020-sinii-103877977-1.jpg',
+    secondImage:
+      'https://resources.cdn-kaspi.kz/shop/medias/sys_master/images/images/h58/h3b/51047882752030/yandeks-mini-2-yndx-00020-sinii-103877977-2.jpg',
+    image3:
+      'https://resources.cdn-kaspi.kz/shop/medias/sys_master/images/images/h34/h6e/49342277877790/yandeks-mini-2-yndx-00020-sinii-103877977-1.jpg',
+    link: 'https://kaspi.kz/shop/p/jandeks-stantsija-mini-2-s-chasami-sinii-103877977/?c=750000000#!/item',
+    rating: 5.0,
+    categoryId: 4,
+  },
+
+  {
+    id: 11,
+    name: 'Chair Qaz Oryndyq',
+    price: 53.79,
+    description: `Chair Qaz Oryndyq AS002, 95x55x55 cm, beige`,
+    image:
+      'https://resources.cdn-kaspi.kz/shop/medias/sys_master/images/images/h7b/h99/62797277397022/stul-klassicheskii-as002-metall-95x55x55-bezhevyi-106633022-1.jpg',
+    secondImage:
+      'https://resources.cdn-kaspi.kz/shop/medias/sys_master/images/images/h36/h2c/62797408927774/stul-klassicheskii-as002-metall-95x55x55-bezhevyi-106633022-2.jpg',
+    image3:
+      'https://resources.cdn-kaspi.kz/shop/medias/sys_master/images/images/h6f/hde/62797409386526/stul-klassicheskii-as002-metall-95x55x55-bezhevyi-106633022-3.jpg',
+    link: 'https://kaspi.kz/shop/p/stul-qaz-oryndyq-as002-95x55x55-sm-bezhevyi-106633022/?c=750000000#!/item',
+    rating: 5.0,
+    categoryId: 2,
+  },
+
+  {
+    id: 12,
+    name: 'Mixer Bosch',
+    price: 62.21,
+    description: `Mixer Bosch MFQ40304 pink`,
+    image:
+      'https://resources.cdn-kaspi.kz/shop/medias/sys_master/images/images/h7c/h4d/31583682789406/bosch-mfq-40304-rozovyi-10900030-1.jpg',
+    secondImage:
+      'https://resources.cdn-kaspi.kz/shop/medias/sys_master/images/images/h53/he3/50234879574046/bosch-mfq-40304-rozovyi-10900030-2.jpg',
+    image3:
+      'https://resources.cdn-kaspi.kz/shop/medias/sys_master/images/images/h04/h5c/50234880032798/bosch-mfq-40304-rozovyi-10900030-3.jpg',
+    link: 'https://kaspi.kz/shop/p/bosch-mfq40304-rozovyi-10900030/?c=750000000#!/item',
+    rating: 5.0,
+    categoryId: 4,
+  },
+
+];
+
+
+/*
+Copyright Google LLC. All Rights Reserved.
+Use of this source code is governed by an MIT-style license that
+can be found in the LICENSE file at https://angular.io/license
+*/
